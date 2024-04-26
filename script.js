9420567fa05273fe8f6327681742986e179df0f1
Fernanda Soares <fernanda.soares@elogroup.com.br>
2024-04-26 16:37:56 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+window.addEventListener("load", (event) => {
+  console.log("page is fully loaded");
+  setCookies();
+});
+
+window.addEventListener("message", (ev) => {
+  console.log(
+    `Evento Recebido: ${ev.type} | Origem: ${ev.origin} | Tipo: ${ev.data?.type}`
+  );
+
+  ev.source.postMessage(ev.data, ev.origin);
+});
+
+// function sendMessage(source, origin, tipo) {
+//   console.log("[[re-enviando mensagens]]");
+//   //window.postMessage({ type: tipo }, origin);
+//   window.postMessage({ type: tipo }, source);
+//   //   source.postMessage({ type: tipo });
+//}
+
+function sendNewMessage() {
+  document
+    .getElementById("my-iframe")
+    .contentWindow.postMessage({ type: "NOVA_MENSAGEM!" }, "*");
+}
+
+function setCookies() {
+  console.log("[[setando cookies]]");
+  document.cookie = "teste=cookie_APP_DE_TESTE; path=/; SameSite=None; Secure";
+}
